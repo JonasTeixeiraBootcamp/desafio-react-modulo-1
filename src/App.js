14f82b69b25f23119f7962cdf8354b3a42317b59
Jonas <jonaslagoteixeira@gmail.com>
2020-04-28 19:30:47 -0300

    foi feito busca dos repositorios ao carregar aplicacao

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './styles.css';
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function loadRepositories() {
+      const response = await api.get('repositories');
+
+      setRepositories(response.data);
+    }
+
+    loadRepositories();
+  }, []);
+
+  async function handleAddRepository() {
+    // TODO
+  }
+
+  async function handleRemoveRepository(id) {
+    // TODO
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(1)}>Remover</button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
